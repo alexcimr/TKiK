7bde07d60f2503bf57818cb762513f76a78469ba
alexx <alexxcimr@gmail.com>
2026-04-12 17:55:43 +0200

    Usuniety reporter

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from reporter import Reporter
+from scanner import Scanner
 
 
 def main():
-    reporter = Reporter()
 
     text = "2 + &&3 * (10 1a / 2)"
-    reporter.print_tokens(text)
 
+    scanner = Scanner()
+    scanner.scan(text)
+    tokens = scanner.get_tokens()
+
+    print("===TOKENY===")
+    for token in tokens:
+        print(token)
+    print("============")
+
+    
     in_file = "dane.txt"
     out_file = "wynik.html"
-    reporter.to_html(in_file, out_file)
+    scanner.to_html(in_file, out_file)
 
 if __name__ == "__main__":
     main()
--- scanner.py
+++ scanner.py
@@ -1,5 +1,6 @@
 from enums import TokenType
 from token import Token
+import html
 
 class Scanner:
     math_chars = {
@@ -9,6 +10,18 @@ class Scanner:
         '/': TokenType.DZIELENIE,
         '(': TokenType.LNAWIAS,
         ')': TokenType.PNAWIAS,
+    }
+
+    COLORS = {
+        TokenType.LICZBA: "green",
+        TokenType.ID: "blue",
+        TokenType.PLUS: "orange",
+        TokenType.MINUS: "purple",
+        TokenType.MNOZENIE: "magenta",
+        TokenType.DZIELENIE: "cyan",
+        TokenType.LNAWIAS: "brown",
+        TokenType.PNAWIAS: "deeppink",
+        TokenType.BLAD: "red"
     }
 
     def __init__(self):
@@ -76,3 +89,39 @@ class Scanner:
 
     def get_tokens(self):
         return self.tokens
+
+    def to_html(self, in_file: str, out_file: str):
+        """Generuje pokolorowany plik HTML na podstawie pliku tekstowego z zachowaniem układu."""
+        with open(in_file, "r", encoding="utf-8") as f:
+            text = f.read()
+
+        self.reset_tokens()
+        self.scan(text)
+        tokens = self.get_tokens()
+
+        html_body = ""
+        current_pos = 0
+
+        # Przechodzenie przez tokeny
+        for t in tokens:
+            while current_pos < t.start_pos:
+                html_body += html.escape(text[current_pos])
+                current_pos += 1
+
+            color = self.COLORS[t.token_type]
+            val = html.escape(str(t.value))
+            html_body += f'<span style="color: {color};">{val}</span>'
+
+            current_pos = t.end_pos
+
+        # Dodawanie bialych znakow po ostatnim tokenie
+        while current_pos < len(text):
+            html_body += html.escape(text[current_pos])
+            current_pos += 1
+
+        with open(out_file, "w", encoding="utf-8") as f:
+            # <pre>: zachowanie układu (znaki białe)
+            # monospace: czcionka stałoszerokościowa
+            # font-size: wielkość czcionki
+            # line-height przerwa między wierszami
+            f.write(f"<pre style='font-family: monospace; font-size: 20px; line-height: 1.5;'>{html_body}</pre>")
